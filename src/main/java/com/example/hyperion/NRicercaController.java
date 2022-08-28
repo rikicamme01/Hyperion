e6df336625bb1ec3fbfd84ccb83ab8805d27e682
riccardo <283824@studenti.unimore.it>
2022-08-28 23:03:58 +0200

    27/08

--- src/main/java/com/example/hyperion/NRicercaController.java
+++ src/main/java/com/example/hyperion/NRicercaController.java
@@ -4,8 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
+import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -13,26 +16,49 @@ import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 
 public class NRicercaController {
+
+
     @FXML
     public Button btCerca;
     @FXML
     public Label lbOrarioTemplate;
     @FXML
+    public Button btSalvaCome;
+    @FXML
     private Pane twitterTemplate;
 
     @FXML
     private Label lbTemplate;
+    @FXML
+    private TextField tfProva;
+    @FXML
+    private Pane PanePadre;
+    @FXML
+    private Pane PaneFiglio;
     int num_tweet = 12;
     int num_reply = 50;
-
     SimpleDateFormat dtf = new SimpleDateFormat("HH:mm · MMM dd, yyyy");
 
     public void cerca(ActionEvent actionEvent) {
         twitterTemplate.setOpacity(1);
         Date date = new Date();
+        lbOrarioTemplate.setText(dtf.format(date));
         num_tweet++;
         num_reply++;
         lbTemplate.setText("Sono stati trovati " + num_tweet + " Tweet\ne " + num_reply + " commenti");
-        lbOrarioTemplate.setText(dtf.format(date));
+
+
+        PanePadre.setVisible(false);
+        //tfProva.clear();
+    }
+
+    public void salvaCome(ActionEvent actionEvent) {
+        System.out.println("pressed salvaCome");
+
+        //PanePadre.getChildren().add(PaneFiglio);
+        //devo pulire i singoli nodi:
+
+        PanePadre.setVisible(true);
+
     }
 }
